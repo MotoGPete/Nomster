fd07afc2bd51919b33170bb5edd340e5f4371c16
Peter Boulton <peterboulton1228@gmail.com>
2019-09-14 19:27:24 +0000

    comments database fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_14_044203) do
+ActiveRecord::Schema.define(version: 2019_09_14_191945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "comments", force: :cascade do |t|
+  create_table "comments", id: :serial, force: :cascade do |t|
     t.text "message"
     t.string "rating"
     t.integer "user_id"
